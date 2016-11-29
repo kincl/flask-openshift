4369fe5740d318dd79c6c525ebfe7eba32322ca2
Jason Kincl <kincljc@ornl.gov>
2016-11-29 13:53:17 -0500

    adding some files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='example',
+      version='1.0',
+      description='Example flask script',
+      author='Jason Kincl',
+      packages=['example'],
+     )
