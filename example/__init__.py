d5b06472ec08b1414dbfc9906ace158c8f257fc6
Jason Kincl <kincljc@ornl.gov>
2016-12-01 14:02:51 -0500

    adding mysql support?

--- example/__init__.py
+++ example/__init__.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
 
+app.config.update(
+    MYSQL_HOST='mysql',
+    MYSQL_USER='user',
+    MYSQL_PASSWORD='password',
+    MYSQL_DB='database'
+)
+
+mysql = MySQL(app)
+
+@app.route('/read_db_users')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT user, host FROM mysql.user''')
+    rv = cur.fetchall()
+    return str(rv)
 
 @app.route("/")
 def hello():
